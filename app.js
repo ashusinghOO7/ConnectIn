d0e7ea638cb2cfbbc410667f68afe127a8f0cdb3
Ashutosh kumar Singh <76557299+ashusinghOO7@users.noreply.github.com>
2022-07-29 15:40:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 mongoose.connect(
-  "mongodb+srv://Ashutosh:ashu2309@cluster0.d2gr3.mongodb.net/?retryWrites=true&w=majority",
+  "mongodb+srv://ashusingh_007:ashu2309@mycluster0.bp0om.mongodb.net/?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
 
